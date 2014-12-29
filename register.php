c7ab9d931716dce7bb611ddb83dbaab7f2b5d4a7
Prikshit <prikshit911@gmail.com>
2014-12-30 01:21:19 +0530

    rem unnecessary headers

--- register.php
+++ register.php
@@ -111,54 +111,10 @@
                                                             'url_strip_qs' => null,
                                                             'preserve_recipients' => null,
                                                             'view_content_link' => null,
-                                                            'bcc_address' => 'message.bcc_address@example.com',
                                                             'tracking_domain' => null,
                                                             'signing_domain' => null,
                                                             'return_path_domain' => null,
-                                                            'merge' => true,
-                                                            'merge_language' => 'mailchimp',
-                                                            'global_merge_vars' => array(
-                                                                array(
-                                                                    'name' => 'merge1',
-                                                                    'content' => 'merge1 content'
-                                                                )
-                                                            ),
-                                                            'merge_vars' => array(
-                                                                array(
-                                                                    'rcpt' => 'recipient.email@example.com',
-                                                                    'vars' => array(
-                                                                        array(
-                                                                            'name' => 'merge2',
-                                                                            'content' => 'merge2 content'
-                                                                        )
-                                                                    )
-                                                                )
-                                                            ),
-                                                            'tags' => array('password-resets'),
-                                                            'subaccount' => 'customer-123',
-                                                            'google_analytics_domains' => array('example.com'),
-                                                            'google_analytics_campaign' => 'message.from_email@example.com',
-                                                            'metadata' => array('website' => 'www.example.com'),
-                                                            'recipient_metadata' => array(
-                                                                array(
-                                                                    'rcpt' => 'recipient.email@example.com',
-                                                                    'values' => array('user_id' => 123456)
-                                                                )
-                                                            ),
-                                                            'attachments' => array(
-                                                                array(
-                                                                    'type' => 'text/plain',
-                                                                    'name' => 'myfile.txt',
-                                                                    'content' => 'ZXhhbXBsZSBmaWxl'
-                                                                )
-                                                            ),
-                                                            'images' => array(
-                                                                array(
-                                                                    'type' => 'image/png',
-                                                                    'name' => 'IMAGECID',
-                                                                    'content' => 'ZXhhbXBsZSBmaWxl'
-                                                                )
-                                                            )
+                                                            'merge' => true
                                                         );
                                                         $async = false;
                                                         $ip_pool = 'Main Pool';
